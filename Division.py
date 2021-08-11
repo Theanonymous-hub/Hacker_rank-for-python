54999ae3f051371c5667cfded57ad7789b67c76c
Prasana.R <76907455+Theanonymous-hub@users.noreply.github.com>
2021-08-11 11:46:55 +0530

    Division.py

---
+++ Division.py
@@ -0,0 +1,17 @@
+#The provided code stub reads two integers,  and , from STDIN.
+#Add logic to print two lines. The first line should contain the result of integer division,  // . The second line should contain the result of float division,  / .
+#No rounding or formatting is necessary.
+#Input Format
+#The first line contains the first integer, .
+#The second line contains the second integer, .
+#Example
+#The result of the integer division 3//5=0 .
+#The result of the float division is 3/5=0.6.
+
+
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a//b);
+    print(a/b);
